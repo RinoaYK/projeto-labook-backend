79ed4c909ba657fcd7e5bb7bac411512a519feb0
RinoaYK <rinoayk@gmail.com>
2023-05-05 13:47:40 -0300

    Criação models, Types, arquitetura e router

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,37 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { userRouter } from "./router/UserRouter";
+import { postRouter } from "./router/PostRouter";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.listen(Number(process.env.PORT) || 3003, () => {
-    console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
-})
+  console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`);
+});
 
-// routers das entidades
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
 
 app.get("/ping", async (req: Request, res: Response) => {
-    try {
-      res.status(200).send({ message: "Pong!" });
-    } catch (error) {
-      console.log(error);
-  
-      if (req.statusCode === 200) {
-        res.status(500);
-      }
-  
-      if (error instanceof Error) {
-        res.send(error.message);
-      } else {
-        res.send("Erro inesperado");
-      }
+  try {
+    res.status(200).send({ message: "Pong!" });
+  } catch (error) {
+    console.log(error);
+
+    if (req.statusCode === 200) {
+      res.status(500);
     }
-  });
+
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
